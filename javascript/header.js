78ca9251468a97667401de1dd397892d0b1775aa
Marcos Alves <marcos.a.lopes7701@gmail.com>
2026-01-13 18:32:22 -0300

    Refatora o código do cabeçalho para melhorar a legibilidade e a consistência

--- javascript/header.js
+++ javascript/header.js
@@ -1,23 +1,26 @@
 class IndexHeader extends HTMLElement {
-  connectedCallback() {
-      this.classList.add('header', 'common-max-w', 'common-pad-w');
-      this.setAttribute('id', 'header')
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.classList.add("header", "common-max-w", "common-pad-w");
+        this.setAttribute("id", "header");
 
-    this.innerHTML = `
-      <div class="header-top">
-        <a href="#header">writings.dev</a>
-        <nav>
-          <a>categories</a>
-          <a>articles</a>
-          <a>about</a>
-        </nav>
-      </div>
-      <div class="header-banner">
-        <img src="public/logo.svg" alt="Logo">
-        <h1>Writings for Developers</h1>
-        <h2>Curated collection of articles for busy developers</h2>
-      </div>
-    `;
-  }
+        this.innerHTML = `
+          <div class="header-top">
+            <a href="#header">writings.dev</a>
+            <nav>
+              <a>categories</a>
+              <a>articles</a>
+              <a>about</a>
+            </nav>
+          </div>
+          <div class="header-banner">
+            <img src="public/logo.svg" alt="Logo">
+            <h1>Writings for Developers</h1>
+            <h2>Curated collection of articles for busy developers</h2>
+          </div>
+        `;
+    }
 }
-customElements.define('custom-header', IndexHeader);
+customElements.define("custom-header", IndexHeader);
