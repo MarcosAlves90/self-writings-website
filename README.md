1a393faa029eb8901cf31ce5eb8a60b5458b0036
Marcos Alves <marcos.a.lopes7701@gmail.com>
2026-01-13 18:11:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Estudo de HTML, CSS e JavaScript Modernos
+
+Este repositório contém um estudo sobre as tecnologias modernas de desenvolvimento web, incluindo HTML, CSS e JavaScript.
---
+++ javascript/header.js
@@ -0,0 +1,22 @@
+class IndexHeader extends HTMLElement {
+  connectedCallback() {
+    this.classList.add('index-header', 'common-max-w', 'common-pad-w');
+
+    this.innerHTML = `
+      <div class="index-header-top">
+        <p>writings.dev</p>
+        <nav>
+          <a>categories</a>
+          <a>articles</a>
+          <a>about</a>
+        </nav>
+      </div>
+      <div class="index-header-banner">
+        <img src="public/logo.svg" alt="Logo">
+        <h1>Writings for Developers</h1>
+        <h2>Curated collection of articles for busy developers</h2>
+      </div>
+    `;
+  }
+}
+customElements.define('index-header', IndexHeader);
