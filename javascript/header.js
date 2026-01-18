1e1f18b5f54edd14e94a5ccde762a1af84ddcc59
Marcos Alves <marcos.a.lopes7701@gmail.com>
2026-01-17 22:41:13 -0300

    adiciona estilos e estrutura ao cabeçalho para melhorar a apresentação e a responsividade

--- javascript/header.js
+++ javascript/header.js
@@ -1,26 +1,48 @@
-class IndexHeader extends HTMLElement {
-    constructor() {
-        super();
-    }
-    connectedCallback() {
-        this.classList.add("header", "common-max-w", "common-pad-w");
-        this.setAttribute("id", "header");
+const html = String.raw;
 
-        this.innerHTML = `
-          <div class="header-top">
-            <a href="#header">writings.dev</a>
-            <nav>
-              <a>categories</a>
-              <a>articles</a>
-              <a>about</a>
-            </nav>
-          </div>
-          <div class="header-banner">
-            <img src="public/logo.svg" alt="Logo">
-            <h1>Writings for Developers</h1>
-            <h2>Curated collection of articles for busy developers</h2>
-          </div>
-        `;
-    }
+class IndexHeader extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.classList.add("header", "common-max-w", "common-pad-w");
+    this.setAttribute("id", "header");
+
+    this.innerHTML = html`
+      <div class="header-top">
+        <a href="#header">writings.dev</a>
+        <nav>
+          <a>categories</a>
+          <a>articles</a>
+          <a>about</a>
+        </nav>
+        <button class="header-top-hamburger common-button">open</button>
+      </div>
+      <div class="header-menu-mobile">
+        <nav>
+          <a class="header-menu-mobile-button common-button">categories</a>
+          <a class="header-menu-mobile-button common-button">articles</a>
+          <a class="header-menu-mobile-button common-button">about</a>
+        </nav>
+      </div>
+      <div class="header-banner">
+        <img src="public/logo.svg" alt="Logo">
+        <h1>Writings for Developers</h1>
+        <h2>Curated collection of articles for busy developers</h2>
+      </div>
+    `;
+
+    const btn = this.querySelector('.header-top-hamburger')
+    const menu = this.querySelector('.header-menu-mobile')
+
+    btn.setAttribute('aria-expanded', 'false')
+
+    btn.addEventListener('click', () => {
+      const opened = menu.classList.toggle('open')
+      btn.setAttribute('aria-expanded', String(opened))
+      btn.textContent = opened ? 'close' : 'open';
+    })
+
+  }
 }
 customElements.define("custom-header", IndexHeader);
